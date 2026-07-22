7f25084fd19e21e8c926c8180202c258b9e61106
Anjali Ramani <ramanianjali258@gmail.com>
2026-07-22 16:16:00 +0530

    Day 25: Learned Bias vs Variance

---
+++ bias_variance_demo.py
@@ -0,0 +1,19 @@
+print("Bias vs Variance")
+
+print("\nHigh Bias:")
+print("- Model is too simple")
+print("- Underfitting")
+print("- Poor training accuracy")
+print("- Poor testing accuracy")
+
+print("\nHigh Variance:")
+print("- Model is too complex")
+print("- Overfitting")
+print("- High training accuracy")
+print("- Poor testing accuracy")
+
+print("\nIdeal Model:")
+print("- Low Bias")
+print("- Low Variance")
+print("- Good training accuracy")
+print("- Good testing accuracy")
